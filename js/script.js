c78bfd7243bb1de1d9c5b38494c88ce31c0b9adb
Fernando Licea <fernandolic1407@gmail.com>
2023-05-17 21:56:56 -0600

    trabajado en backend

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// function demoDisplay() {
+//     document.getElementById("myid").style.visibility = "hidden";
+// }
+
+function encriptar(){
+
+    let x = document.getElementById("textIngresar").value;
+
+    s = x;
+
+    let text = s;
+
+    document.getElementById("mostrando").innerHTML = text;
+    document.getElementById("textIngresar").style.visibility = "hidden";
+}
+
+
+
